d19846a59b206dd68341d57614fff3d7de1756a3
FelipeDominguesB <felipedom2001@hotmail.com>
2022-09-09 13:13:47 -0300

    Fixing format amount bug

--- Atividades/A2/main.js
+++ Atividades/A2/main.js
@@ -114,9 +114,9 @@ const DOM = {
 
 const Utils = {
     formatAmount(value){
-        value = Number(value.replace(/\,\./g, "")) * 100
+        value = value * 100
         
-        return value
+        return Math.round(value)
     },
 
     formatDate(date) {
