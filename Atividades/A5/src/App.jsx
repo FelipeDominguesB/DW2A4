dc521c82ffbed920f24fc1e44e1d43e07dfb3950
FelipeDominguesB <felipedom2001@hotmail.com>
2022-10-07 13:48:31 -0300

    added gitiginore

---
+++ Atividades/A5/src/App.jsx
@@ -0,0 +1,4 @@
+export function App()
+{
+    return <h1>Hello World!</h1>
+}
---
+++ Atividades/A5/src/index.jsx
@@ -0,0 +1,5 @@
+import {render} from "react-dom";
+import { App } from "./App";
+
+render(<h1>Hello World</h1>, document.getElementById('root'));
+
